b3321c3ae09dd141de58e933b394b2b87a37944d
siamsarker <sarkersiam2@gmail.com>
2023-03-28 01:55:16 +0600

    cart functionality added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import './Cart.css';
 
 const Cart = ({cart}) => {
+    let total = 0;
+    let shipping = 0;
+    let grandTotal = 0;
+    for (const product of cart) {
+        total += product.price;
+        shipping += product.shipping;
+    }
+    const tax = (total * 0.1).toFixed(2);
+    grandTotal = total + shipping + parseFloat(tax);
+
     return (
-        <div>
+        <div className='cart'>
             <h4>Order Summery</h4>
             <p>Selected Items: {cart.length}</p>
+            <p>Total price: {total}</p>
+            <p>Total Shipping: {shipping}</p>
+            <p>Tax: {tax}</p>
+            <p>Grand Total: {grandTotal}</p>
         </div>
     );
 };
